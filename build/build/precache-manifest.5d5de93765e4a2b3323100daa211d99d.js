17646f8fc38f75dd819b82c87231c5b7e8ccf52c
Pawan Singh <63010043+pawan2806@users.noreply.github.com>
2020-09-15 02:15:52 +0530

    Add files via upload

---
+++ build/build/precache-manifest.5d5de93765e4a2b3323100daa211d99d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5215d001f90fc91c8fb8e4a87889406f",
+    "url": "/pokedex-starter/index.html"
+  },
+  {
+    "revision": "5b26407cfd02b4dc3738",
+    "url": "/pokedex-starter/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e7b412ad72e2955667e5",
+    "url": "/pokedex-starter/static/css/main.362d1dca.chunk.css"
+  },
+  {
+    "revision": "5b26407cfd02b4dc3738",
+    "url": "/pokedex-starter/static/js/2.831677f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex-starter/static/js/2.831677f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b412ad72e2955667e5",
+    "url": "/pokedex-starter/static/js/main.1c958c26.chunk.js"
+  },
+  {
+    "revision": "dabd9225ae6ae6ead6e8",
+    "url": "/pokedex-starter/static/js/runtime-main.97e7410b.js"
+  },
+  {
+    "revision": "71761a0e4f7f2475e66fa40c269789b4",
+    "url": "/pokedex-starter/static/media/Big John.71761a0e.woff"
+  }
+]);
